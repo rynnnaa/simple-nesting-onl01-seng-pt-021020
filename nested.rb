111f5c3cc6bf155348ae2cef0606e1865754edfd
Ryna Truong <rynatruong@outlook.com>
2020-02-23 23:13:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     return programmer_hash
 
 end
