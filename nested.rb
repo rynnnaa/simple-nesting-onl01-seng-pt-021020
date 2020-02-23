01f56142deb2e5c57715dce671f73ba9b6acb467
Ryna Truong <rynatruong@outlook.com>
2020-02-23 23:06:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,10 +77,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
      programmer_hash = {
- :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
+       :yukihiro_matsumoto => {
+         :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+       }
+     }
+  
  }
 
     
